dd1864e49fe1e257b1c07911b364aba916f92396
Michael Engelhard <engelhard.michael+git@gmail.com>
2023-11-02 13:28:46 +0100

    Add button enable

--- build.py
+++ build.py
@@ -13,13 +13,16 @@ with open("README.md", "r") as f:
     readme = f.read()
 
 links = [build_link(path) for path in Path(".").glob("*.js")]
+links_combined = "\n\n".join(links)
+new_readme = re.sub(
+    "(<!--- links start --->).*(<!--- links end --->)",
+    f"\\1\n{links_combined}\n\\2",
+    readme,
+    flags=re.DOTALL,
+)
 
 with open("README.md", "w") as f:
-    links_combined = "\n\n".join(links)
-    new_readme = re.sub(
-        "(<!--- links start --->).*(<!--- links end --->)",
-        f"\\1\n{links_combined}\n\\2",
-        readme,
-        flags=re.DOTALL,
-    )
     f.write(new_readme)
+
+with open("README.html", "w") as f:
+    f.write(f"<html><body>{new_readme}</body></html>")
---
+++ enable.js
@@ -0,0 +1,5 @@
+javascript: (function () {
+  for (let button of document.querySelectorAll("button")) {
+    button.disabled = false;
+  }
+})();
